b5b460e9c51177dac9063a5519b0aff792f989f2
Nikhil Jain <13591875+N1kh1l-Ja1n@users.noreply.github.com>
2019-07-15 19:32:25 +0530

    Added new file

---
+++ src/GFG/GFG.java
@@ -0,0 +1,12 @@
+package com.example.gfg;
+
+//A java program to print GeeksForGeeks
+public class GFG {
+	public static void main(String[] args) {
+		
+		System.out.println("Geeks For Geeks");
+		
+	}
+	
+
+}
