fc79084a84707a8ec9e7fbf41dc7819b7fa4e005
Nikhil <jainnik26@gmail.com>
2019-07-18 19:41:54 +0530

    Loader class added

---
+++ src/GFG/Loader.java
@@ -0,0 +1,16 @@
+package com.example.gfg;
+
+//class loader, bootstrap class loader, a platform specific machine instructions that kick off the whole classloading process.
+
+public class Loader {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(String.class.getClassLoader());
+		
+		System.out.println(Loader.class.getClassLoader());
+	}
+	
+	
+
+}
