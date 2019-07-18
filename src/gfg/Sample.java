c0d3d52bb6edfa060900f37d6282eb53a08414d7
Nikhil <jainnik26@gmail.com>
2019-07-18 14:22:21 +0530

    New files added

---
+++ src/gfg/Sample.java
@@ -0,0 +1,15 @@
+package com.example.gfg;
+
+public class Sample {
+
+}
+class student{
+	
+}
+class Test{
+	public static void main (String[] args) {
+		System.out.println("Class File Structure");
+	}
+	
+}
+
