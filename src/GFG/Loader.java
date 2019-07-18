1064e69be127bf052f59512edd74a2c019d11eac
Nikhil Jain <13591875+N1kh1l-Ja1n@users.noreply.github.com>
2019-07-18 19:53:59 +0530

    Update Loader.java

--- src/GFG/Loader.java
+++ src/GFG/Loader.java
@@ -7,8 +7,9 @@ public class Loader {
 	public static void main(String[] args) {
 		
 		System.out.println(String.class.getClassLoader());
-		
+//This print out null since Bootstrap class loader is not a java object		
 		System.out.println(Loader.class.getClassLoader());
+//This print out system class loader.		
 	}
 	
 	
